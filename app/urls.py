97839de4758e806b20177805c8f61e1b92440d7f
Jiayue Wang <2878524w@student.gla.ac.uk>
2025-03-12 12:36:17 +0000

    add about page

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from app import views
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.index, name=
-    'index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
