e685871bd73899c1ab80c193ee168282bd4a27c6
Jiayue Wang <2878524w@student.gla.ac.uk>
2025-03-12 11:43:53 +0000

    add some basic templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
 def index(request):
-    return HttpResponse("Heyyyy")
+    respnse = render(request, 'restabook/index.html')
+    return respnse
