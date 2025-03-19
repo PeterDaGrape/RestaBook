ff9eaa019aac06671fa2c281cf58aaa551f09f6c
Jiayue Wang <2878524w@student.gla.ac.uk>
2025-03-19 10:52:06 +0000

    basic admin

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from app.models import User, Cuisine, Restaurant, Booking, Review, StandardHours, CustomHours
 
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manager', 'cuisine', 'email', 'address', 'phone')
+
+admin.site.register(Restaurant, RestaurantAdmin)
 admin.site.register(User)
 admin.site.register(Cuisine)
-admin.site.register(Restaurant)
 admin.site.register(Booking)
 admin.site.register(Review)
 admin.site.register(StandardHours)
